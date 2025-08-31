4b625ab626f5ad3fa00e583678d4adf19e4b8032
Michael Okang <michaelokang8@gmail.com>
2025-08-31 22:55:49 +0000

    Delete LibraryProject directory

--- LibraryProject/bookshelf/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)       # Book title
-    author = models.CharField(max_length=100)      # Book author
-    publication_year = models.IntegerField()       # Year of publication
-
-    def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
