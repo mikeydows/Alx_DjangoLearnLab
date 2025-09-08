ee0b3a5ffee4e7e0b6b3a2a277b6841e923e7445
Michael Okang <michaelokang8@gmail.com>
2025-09-08 00:20:50 +0000

    Add files via upload

---
+++ django_models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("books/", views.list_books, name="list_books"),  # function-based view
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+]
+from django.urls import path
+from .views import register, CustomLoginView, CustomLogoutView, list_books, LibraryDetailView
+
+urlpatterns = [
+    # Book + Library Views
+    path("books/", list_books, name="list_books"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication Views
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("register/", register, name="register"),
+     path("admin-view/", views.admin_view, name="admin_view"),
+    path("librarian-view/", views.librarian_view, name="librarian_view"),
+    path("member-view/", views.member_view, name="member_view"),
+]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("books/add/", views.add_book, name="add_book"),
+    path("books/<int:book_id>/edit/", views.edit_book, name="edit_book"),
+    path("books/<int:book_id>/delete/", views.delete_book, name="delete_book"),
+]
