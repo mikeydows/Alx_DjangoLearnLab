f11e1e7e7812df2274c5fc9efeffaced0823f777
Michael Okang <michaelokang8@gmail.com>
2025-10-05 09:37:50 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -11,6 +11,8 @@ class Post(models.Model):
         return self.title
 
 
+from .models import Post
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,7 +21,11 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Comment by {self.author} on {self.post}"
+        return f'Comment by {self.author} on {self.post}'
+
+    def get_absolute_url(self):
+        return self.post.get_absolute_url()
+
 
 
 class Tag(models.Model):
@@ -28,3 +34,4 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
