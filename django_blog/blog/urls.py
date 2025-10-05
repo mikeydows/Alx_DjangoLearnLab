a158742350adea2e8fba10ff9c933d23f3c0e8ca
Michael Okang <michaelokang8@gmail.com>
2025-10-05 12:28:50 +0000

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     # Posts
-    path('', views.PostListView.as_view(), name='post-list'),
+        path('', views.PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete')
 
     # ✅ Comments (exact matches for checker)
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
@@ -25,5 +25,6 @@ urlpatterns = [
     path('tags/<slug:tag_slug>/', views.tag_posts, name='tag_posts'),
     path('search/', views.search_posts, name='search_posts'),
 ]
+
 
 
