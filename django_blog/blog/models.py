5e2d3eb57a8bf0f3e2ce83f1428187d3359af72b
Michael Okang <michaelokang8@gmail.com>
2025-10-05 10:04:19 +0000

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .models import Post
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)  # ✅ Tag integration
 
     def __str__(self):
         return self.title
 
 
-from .models import Post
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
@@ -30,8 +32,9 @@ class Comment(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    posts = models.ManyToManyField(Post, related_name='tags')
 
     def __str__(self):
         return self.name
+
+
 
