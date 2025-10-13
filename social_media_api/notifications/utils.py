c0bac821d2931030eec89ffc8d759b94d97057e1
Michael Okang <michaelokang8@gmail.com>
2025-10-13 00:10:10 +0000

    Delete social_media_api directory

--- social_media_api/notifications/utils.py
+++
@@ -1,9 +0,0 @@
-from .models import Notification
-from django.contrib.contenttypes.models import ContentType
-
-def create_notification(recipient, actor, verb, target=None):
-    kwargs = {"recipient": recipient, "actor": actor, "verb": verb}
-    if target is not None:
-        kwargs["target_content_type"] = ContentType.objects.get_for_model(target.__class__)
-        kwargs["target_object_id"] = target.id
-    Notification.objects.create(**kwargs)
